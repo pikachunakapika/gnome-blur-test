c5d2260be2555ab8990b6b1913949b5afbcd4de1
pikachunakapika <boomschakalaka_18@hotmail.com>
2020-10-18 18:32:15 +0200

    Implementation

--- extension.js
+++ extension.js
@@ -16,16 +16,89 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-/* exported init */
+/* exported init */ 
+
+/*
+
+Reference & Credits to:
+- https://github.com/CorvetteCole/blur-provider
+
+*/
+
+const { Clutter, Shell, St } = imports.gi;
+const Main = imports.ui.main;
+const GrabHelper = imports.ui.grabHelper;
 
 class Extension {
     constructor() {
     }
 
     enable() {
+
+        
+        let blurEffect = new Shell.BlurEffect({sigma: 15, mode: Shell.BlurMode.BACKGROUND});
+        this.blurActor = new Clutter.Actor({visible: true});
+        
+        this.blurActor.add_effect_with_name('blur-effect', blurEffect);
+        
+        this.widget = new St.Widget({
+            style: 'background-color: rgba(100, 100, 100, 0.2); border-radius: 200px',
+            x: 600, y: 600,
+            width: 400, height: 400,
+            reactive: true
+        });
+
+
+        this.blurActor.add_constraint(new Clutter.BindConstraint({
+            source: this.widget,
+            coordinate: Clutter.BindCoordinate.ALL,
+        }));
+
+        this.isMouseDown = false;
+
+        this.widget.connect('button-press-event', (event, user_data) => {
+            let [global_x, global_y] = global.get_pointer();
+            [this.offsetX, this.offsetY] = [global_x - this.widget.x, global_y - this.widget.y];
+            print(global_x, global_y, this.widget.x, this.widget.y);
+            this.isMouseDown = true;
+            print("PRESS");
+            this._grabHelper.grab({ actor: this.widget, onUngrab: () => print("UNGRAB") });
+
+        });
+
+        global.stage.connect('motion-event', () => {
+            if (this.isMouseDown) {
+                let [x, y] = global.get_pointer();
+                this.widget.set_x(x - this.offsetX);
+                this.widget.set_y(y - this.offsetY);
+                print("MOTION");
+            }
+        });
+
+        global.stage.connect('button-release-event', () => {
+            this.isMouseDown = false;
+            print("RELEASE");
+            this._grabHelper.ungrab();
+        });
+        
+        Main.layoutManager.addChrome(this.widget);
+        Main.uiGroup.insert_child_below(this.blurActor, this.widget);
+
+        this._grabHelper = new GrabHelper.GrabHelper(this.widget);
+        this._grabHelper.addActor(Main.uiGroup);
+        //this._grabHelper.addActor(this.widget);
+        
+
+
+        this.widget.show();
+
     }
 
     disable() {
+        if (this.widget) {
+            this.blurActor.destroy();
+            this.widget.destroy();
+        }
     }
 }
 
