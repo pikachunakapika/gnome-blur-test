92ee872fa09a81acef330aec7b8393926dc6be52
pikachunakapika <boomschakalaka_18@hotmail.com>
2020-10-20 12:34:35 +0200

    Cleanup and additions
    - Add blur for dash-to-dock if found
    - Remove extra actor for blur-effect

--- extension.js
+++ extension.js
@@ -16,87 +16,117 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-/* exported init */ 
-
-/*
-
-Reference & Credits to:
-- https://github.com/CorvetteCole/blur-provider
-
-*/
+ /* exported init */ 
 
 const { Clutter, Shell, St } = imports.gi;
 const Main = imports.ui.main;
 const GrabHelper = imports.ui.grabHelper;
 
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const ExtensionManager = Main.extensionManager;
+
+const DashToDock = ExtensionManager.lookup('dash-to-dock@micxgx.gmail.com');
+
+print(DashToDock);
+
+
+
 class Extension {
     constructor() {
     }
 
-    enable() {
-
-        
+    add_blur_effect(target) {
         let blurEffect = new Shell.BlurEffect({sigma: 20, mode: Shell.BlurMode.BACKGROUND});
-        this.blurActor = new Clutter.Actor({visible: true});
+        target.add_effect_with_name('blur-effect', blurEffect);
+    }
+
+    // Don't thinks is extra actor for blur alone is needed...
+    /*
+    add_blur_actor(target, container) {
+
+        if (!container) 
+            container = target;
         
-        this.blurActor.add_effect_with_name('blur-effect', blurEffect);
-        
-        this.widget = new St.Widget({
+        let blurActor = new Clutter.Actor({visible: true, width: target.width, height: target.height});
+        container.add_actor(blurActor);
+
+        blurActor.add_constraint(new Clutter.BindConstraint({
+            source: target,
+            coordinate: Clutter.BindCoordinate.ALL
+        }));
+
+        this.add_blur_effect(target);
+    }
+    */
+
+    create_draggable_box() {
+
+        let widget = new St.Widget({
             style: 'background-color: rgba(100, 100, 100, 0.2);', /* border-radius: 200px = ugly blurred round corner background :( */
             x: 600, y: 600,
             width: 400, height: 400,
             reactive: true
         });
 
-
-        this.blurActor.add_constraint(new Clutter.BindConstraint({
-            source: this.widget,
-            coordinate: Clutter.BindCoordinate.ALL,
-        }));
+        this._grabHelper = new GrabHelper.GrabHelper(widget);
 
         this.isMouseDown = false;
 
-        this.widget.connect('button-press-event', (event, user_data) => {
+        widget.connect('button-press-event', (event, user_data) => {
             let [global_x, global_y] = global.get_pointer();
-            [this.offsetX, this.offsetY] = [global_x - this.widget.x, global_y - this.widget.y];
-            print(global_x, global_y, this.widget.x, this.widget.y);
+            [this.offsetX, this.offsetY] = [global_x - widget.x, global_y - widget.y];
             this.isMouseDown = true;
-            print("PRESS");
-            this._grabHelper.grab({ actor: this.widget, onUngrab: () => print("UNGRAB") });
+            this._grabHelper.grab({ actor: widget});
 
         });
 
         global.stage.connect('motion-event', () => {
             if (this.isMouseDown) {
                 let [x, y] = global.get_pointer();
-                this.widget.set_x(x - this.offsetX);
-                this.widget.set_y(y - this.offsetY);
-                print("MOTION");
+                widget.set_x(x - this.offsetX);
+                widget.set_y(y - this.offsetY);
             }
         });
 
         global.stage.connect('button-release-event', () => {
             this.isMouseDown = false;
-            print("RELEASE");
             this._grabHelper.ungrab();
         });
         
-        Main.layoutManager.addChrome(this.widget);
-        Main.uiGroup.insert_child_below(this.blurActor, this.widget);
-
-        this._grabHelper = new GrabHelper.GrabHelper(this.widget);
-        this._grabHelper.addActor(Main.uiGroup);
-        //this._grabHelper.addActor(this.widget);
+        Main.layoutManager.addChrome(widget);
         
+        this._grabHelper.addActor(Main.uiGroup);
 
+        return widget;
+    }
 
+    enable() {
+
+        this.widget = this.create_draggable_box();
+        this.add_blur_effect(this.widget);
         this.widget.show();
+
+        if (DashToDock && DashToDock.imports) {
+            
+            const docking = DashToDock.imports.docking;
+            if (docking) {
+                const dockManager = docking.DockManager.getDefault();
+
+                if (dockManager && dockManager.mainDock && dockManager.mainDock.dash && dockManager.mainDock.dash._container) {
+                    dockManager.mainDock.dash._container.set_offscreen_redirect(0);
+                    //this.add_blur_actor(dockManager.mainDock, dockManager.mainDock.dash._container);
+                    this.add_blur_effect(dockManager.mainDock.dash);
+                    //dockManager.mainDock.dash.style = 'background-color: blue';
+                }
+            }
+        }
 
     }
 
     disable() {
+        
         if (this.widget) {
-            this.blurActor.destroy();
             this.widget.destroy();
         }
     }
